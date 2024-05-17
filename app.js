4a55e036b3758d5359ec0adf1324a88982820df0
Raph43lD3V <raphael-lacerda75@hotmail.com>
2024-05-17 14:57:05 -0300

    adicionando quinto-modulo

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 
 //let paragrafo = document.querySelector('p');
 //paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativa = 1;
 //console.log(`o numero secreto é ${numeroSecreto}`)
@@ -16,12 +18,13 @@ function exibirMensagemInicial(){
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
 
+exibirMensagemInicial();
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
     if(chute == numeroSecreto){
         exibirTextoNaTela('h1', 'Acertou!');
-        let palavraTentativa = tentativa > 1 ? 'tentativas' : 'temtativa';
+        let palavraTentativa = tentativa > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `você descobriu o número secreto com ${tentativa} ${palavraTentativa}`;
         exibirTextoNaTela('p', mensagemTentativas);
         document.getElementById('reiniciar').removeAttribute('disabled');
@@ -36,15 +39,28 @@ function verificarChute() {
         limparCampo();    
     }
 }
+function gerarNumeroAleatorio(){
+    let NumeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if(quantidadeDeElementosNaLista == 3){
+        listaDeNumerosSorteados = [];
+    }
+
+    if(listaDeNumerosSorteados.includes(NumeroEscolhido)){
+        return gerarNumeroAleatorio();
+    } else{
+        listaDeNumerosSorteados.push(NumeroEscolhido);
+        console.log(listaDeNumerosSorteados);
+        return NumeroEscolhido;
+    }
+}
 function limparCampo(){
     chute = document.querySelector('input');
     chute.value = '';
 }
-function gerarNumeroAleatorio(){
-    return parseInt(Math.random() * 10 +1);
-}
 function reiniciarJogo(){
-    numeroSecreto =gerarNumeroAleatorio;
+    numeroSecreto = gerarNumeroAleatorio();
     limparCampo();
     tentativa = 1;
     exibirMensagemInicial();
